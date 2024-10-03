a56cd2e2441ac829fb2a5871bb578c47402f6902
Тимур Платонов <gered372@gmail.com>
2024-10-03 18:35:55 +0300

    определена структура обработчика команд

---
+++ Handler.h
@@ -0,0 +1,28 @@
+#pragma once
+#include<iostream>
+class CommandHandler {
+public:
+	static void execute(const std::string& command) {
+		if (command == "ls") {
+
+		}
+		if (command == "cd") {
+
+		}
+		if (command == "exit") {
+
+		}
+		if (command == "chmod") {
+
+		}
+		if (command == "wc") {
+
+		}
+	}
+
+	static void ls{}
+	static void cd{}
+	static void exit{}
+	static void chmod{}
+	static void wc{}
+};
