a064c87d0a5ee90ea559ee10b2181d4fb278e141
Тимур Платонов <gered372@gmail.com>
2024-10-08 15:53:05 +0300

    релизация файловой системы в виде дерева узлов

--- fileSysr.h
+++ fileSysr.h
@@ -1,6 +1,36 @@
 #pragma once
 #include<string>
+#include<vector>
 
 class FileSystem {
 
+	
+
+	public:
+		struct Node {
+			std::string name;
+			std::string content;
+			std::vector<Node*> children;
+			int permissions;
+			bool isDirectory;
+		};
+
+		FileSystem() {
+			root = new Node{ "", "", {}, 755, true };
+			current = root;
+		}
+
+		Node* getRoot() const {
+			return root;
+		}
+		Node* getCurrent() const {
+			return current;
+		}
+		void setCurrent(Node* dir) {
+			current = dir;
+		}
+
+	private:
+		Node* root;
+		Node* current;
 };
